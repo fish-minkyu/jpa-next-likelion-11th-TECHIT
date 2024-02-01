6db0110ad6cfa25d5527eeef719a730f6f9c8cd8
fish-minkyu <e951219@naver.com>
2024-02-01 11:31:52 +0900

    create: 0131 1400 lecture

---
+++ src/main/java/com/example/jpanext/article/entity/Comment.java
@@ -0,0 +1,19 @@
+package com.example.jpanext.article.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String writer;
+}
